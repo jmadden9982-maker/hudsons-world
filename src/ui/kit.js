5eed9fa7d8ce44563673ef4fb0f5d35b236952e6
jmadden9982-maker <jmadden9982@gmail.com>
2026-06-14 21:26:57 +0100

    Remove duplicate addBottomDock export + fix onChange diagnostic stub in ui/kit.js

--- src/ui/kit.js
+++ src/ui/kit.js
@@ -1,9 +1,1 @@
-import Phaser from 'phaser';
-
-const S = { coins: 0, stars: 0, level: 1 };
-const addCoins = () => {};
-const addStars = () => {};
-const onChange = () => {};
-const TITLES = [];
-const xpNeed = () => 100;
-const SFX = { tap: () => {} };
+const onChange = () => () => {};
