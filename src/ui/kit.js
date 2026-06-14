fb443c76bf8d5e6991f07b6cd8617e3d7faea05e
jmadden9982-maker <jmadden9982@gmail.com>
2026-06-14 21:21:09 +0100

    TEMP DIAGNOSTIC: Stub state/audio imports in ui/kit.js to isolate export issue

--- src/ui/kit.js
+++ src/ui/kit.js
@@ -1,1 +1,9 @@
-export { addBottomDock };
+import Phaser from 'phaser';
+
+const S = { coins: 0, stars: 0, level: 1 };
+const addCoins = () => {};
+const addStars = () => {};
+const onChange = () => {};
+const TITLES = [];
+const xpNeed = () => 100;
+const SFX = { tap: () => {} };
